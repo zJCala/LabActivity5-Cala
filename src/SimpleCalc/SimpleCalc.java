1c0bfa84d42132eba16810aabfe3a1c689a943d7
zJCala <jobeth.cala@cit.edu>
2022-12-17 21:48:57 +0800

    added SimpleCalc main function

--- src/SimpleCalc/SimpleCalc.java
+++ src/SimpleCalc/SimpleCalc.java
@@ -2,9 +2,13 @@ package SimpleCalc;
 
 import javax.swing.*;
 
-public class SimpleCalc {
+public class SimpleCalc extends JFrame{
     private JPanel panel1;
     private JTextField textField1;
     private JComboBox comboBox1;
     private JButton computeResultButton;
+
+    public static void main(String[] args) {
+
+    }
 }
