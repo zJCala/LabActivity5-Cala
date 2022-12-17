5fbd9e71fbd4466283e7629d57351a9d79639e10
zJCala <jobeth.cala@cit.edu>
2022-12-17 21:59:49 +0800

    added FoodOrder main Function

--- src/FoodOrdering/FoodOrder.java
+++ src/FoodOrdering/FoodOrder.java
@@ -2,10 +2,14 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrder {
+public class FoodOrder extends JFrame{
     private JPanel panel1;
     private JCheckBox pizzaCheckBox;
     private JRadioButton noneRadioButton;
     private JButton orderButton;
 
+
+    public static void main(String[] args) {
+
+    }
 }
