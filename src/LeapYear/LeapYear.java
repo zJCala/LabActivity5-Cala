29a435ef7735ccccae227c4602359d88add808f5
zJCala <jobeth.cala@cit.edu>
2022-12-17 21:37:34 +0800

    added Leapyear Main function

--- src/LeapYear/LeapYear.java
+++ src/LeapYear/LeapYear.java
@@ -2,8 +2,12 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYear {
+public class LeapYear extends JFrame{
     private JPanel panel1;
     private JTextField textField1;
     private JButton checkButton;
+
+    public static void main(String[] args) {
+
+    }
 }
