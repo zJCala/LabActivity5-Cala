a6176c95e009f2aed20612a9725a05ccf266d19a
zJCala <jobeth.cala@cit.edu>
2022-12-17 21:38:12 +0800

    Progress Leap Year

--- src/LeapYear/LeapYear.java
+++ src/LeapYear/LeapYear.java
@@ -8,6 +8,11 @@ public class LeapYear extends JFrame{
     private JButton checkButton;
 
     public static void main(String[] args) {
-
+        LeapYear year = new LeapYear();
+        year.setTitle("Leap Year Checker");
+        year.setContentPane(year.panel1);
+        year.setSize(400, 300);
+        year.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        year.setVisible(true);
     }
 }
